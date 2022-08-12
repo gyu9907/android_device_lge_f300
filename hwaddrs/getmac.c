e31585cd1d1d1439dee2d257b587b4810e740147
gyu9907 <gyu9907@gmail.com>
2022-08-13 03:08:33 +0900

    WiFi and Bluetooth Ramdom MAC Address

--- hwaddrs/getmac.c
+++ hwaddrs/getmac.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2011 The CyanogenMod Project
+ * Copyright (C) 2011-2015 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,94 +26,98 @@
 #include <time.h>
 #include <unistd.h>
 
-/* Read plain address from NV, if invalid generate random.
- * Set the BT MAC address accordingly */
+/* Read plain address from misc partiton and set the Wifi and BT mac addresses accordingly */
 
 // Validates the contents of the given file
-int checkAddr(char* filepath) {
-    char charbuf[17];
-    int i, notallzeroes = 0, ret = 0;
-    int checkfd = open(filepath, O_RDONLY);
+int checkAddr(char* filepath, int key) {
+	chmod(filepath,S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+	char charbuf[17];
+	int i, notallzeroes = 0, ret = 0;
+	int checkfd = open(filepath, O_RDONLY);
 
-    if (checkfd < 0) return 0; // doesn't exist/error
+	if (checkfd < 0) return 0; // doesn't exist/error
 
-    do {
-        if (read(checkfd, charbuf, 17) != 17) break;
-        for (i = 0; i < 17; i++) {
-            if (i % 3 != 2) {
-                if (!isxdigit(charbuf[i])) break;
-                if (charbuf[i] != '0') notallzeroes = 1;
-            } else if (charbuf[i] != ':') break;
-        }
-        ret = notallzeroes;
-    } while (0);
+	do {
+		if (key == 1) {
+			if (read(checkfd, charbuf, 14) != 14) break;
+			if (strncmp(charbuf, "cur_etheraddr=", 14) != 0) break;
+		}
 
-    close(checkfd);
-    return ret;
+		if (read(checkfd, charbuf, 17) != 17) break;
+		for (i = 0; i < 17; i++) {
+			if (i % 3 != 2) {
+				if (!isxdigit(charbuf[i])) break;
+				if (charbuf[i] != '0') notallzeroes = 1;
+			} else if (charbuf[i] != ':') break;
+		}
+		ret = notallzeroes;
+	} while (0);
+
+	close(checkfd);
+	return ret;
 }
 
 // Writes a file using an address from the misc partition
 // Generates a random address if the one read contains only zeroes
-void writeAddr(char* filepath, int offset) {
-    uint8_t macbytes[6];
-    char macbuf[19];
-    int i, macnums = 0;
-    int miscfd = open("/dev/block/platform/msm_sdcc.1/by-name/misc", O_RDONLY);
-    int writefd = open(filepath, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+void writeAddr(char* filepath, int key) {
+	uint8_t macbytes[6];
+	char macbuf[19];
+	int writefd = open(filepath, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
 
-    lseek(miscfd, offset, SEEK_SET);
+	// Last two bits of the first octet are special
+	macbytes[0] = ((uint8_t) rand() % 256) << 2;
+	macbytes[1] = (uint8_t) rand() % 256;
+	macbytes[2] = (uint8_t) rand() % 256;
+	macbytes[3] = (uint8_t) rand() % 256;
+	macbytes[4] = (uint8_t) rand() % 256;
+	macbytes[5] = (uint8_t) rand() % 256;
 
-    for (i = 0; i < 6; i++) {
-        read(miscfd, &macbytes[i], 1);
-        macnums |= macbytes[i];
-    }
-
-    close(miscfd);
-
-    if (macnums == 0) {
-        // Generate random address, oreo hal style
-        macbytes[0] = 0x22;
-        macbytes[1] = 0x22;
-        macbytes[2] = (uint8_t) rand() % 256;
-        macbytes[3] = (uint8_t) rand() % 256;
-        macbytes[4] = (uint8_t) rand() % 256;
-        macbytes[5] = (uint8_t) rand() % 256;
-    }
-
-    sprintf(macbuf, "%02x:%02x:%02x:%02x:%02x:%02x\n",
-            macbytes[0], macbytes[1], macbytes[2], macbytes[3], macbytes[4], macbytes[5]);
-    write(writefd, &macbuf, 18);
-    close(writefd);
+	if (key == 1) write(writefd, "cur_etheraddr=", 14);
+	sprintf(macbuf, "%02x:%02x:%02x:%02x:%02x:%02x\n",
+			macbytes[0], macbytes[1], macbytes[2], macbytes[3], macbytes[4], macbytes[5]);
+	write(writefd, &macbuf, 18);
+	close(writefd);
+	chmod(filepath,S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
 }
 
 // Simple file copy
 void copyAddr(char* source, char* dest) {
-    char buffer;
-    int sourcefd = open(source, O_RDONLY);
-    int destfd = open(dest, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+	char buffer;
+	int sourcefd = open(source, O_RDONLY);
+	int destfd = open(dest, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
 
-    if (sourcefd < 0 || destfd < 0) return; // doesn't exist/error
+	if (sourcefd < 0 || destfd < 0) return; // doesn't exist/error
 
-    while (read(sourcefd, &buffer, 1) != 0) {
-        write(destfd, &buffer, 1);
-    }
+	while (read(sourcefd, &buffer, 1) != 0) {
+		write(destfd, &buffer, 1);
+	}
 
-    close(sourcefd);
-    close(destfd);
+	close(sourcefd);
+	close(destfd);
+	chmod(dest,S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
 }
 
-int main() {
-    char *datavendorpath, *persistpath;
-    srand(time(NULL));
+int main() {	
+	char *datamiscpath, *persistpath;
+	srand(time(NULL));
 
-    datavendorpath = "/data/misc/bluetooth/bdaddr";
-    persistpath = "/persist/.bdaddr";
-    if (checkAddr(datavendorpath) == 0) {
-        if (checkAddr(persistpath) == 0) {
-            writeAddr(persistpath, 0x4000);
-        }
-        copyAddr(persistpath, datavendorpath);
-    }
+	datamiscpath = "/data/misc/wifi/config";
+	persistpath = "/persist/wifi/.macaddr";
+	if (checkAddr(datamiscpath, 1) == 0) {
+		if (checkAddr(persistpath, 1) == 0) {
+			writeAddr(persistpath, 1);
+		}
+		copyAddr(persistpath, datamiscpath);
+	}
 
-    return 0;
+	datamiscpath = "/data/misc/bluetooth/bdaddr";
+	persistpath = "/persist/.bdaddr";
+	if (checkAddr(datamiscpath, 0) == 0) {
+		if (checkAddr(persistpath, 0) == 0) {
+			writeAddr(persistpath, 0);
+		}
+		copyAddr(persistpath, datamiscpath);
+	}
+	
+	return 0;
 }
