c16ae3dbac46ce395dbc9dfeedfed4dba6a315fb
gyu9907 <gyu9907@gmail.com>
2023-06-19 22:32:20 +0900

    set proprietary-files for f300
    Change-Id: If942bac0bcfd4e3efdfcc688757b31dc85397c7d

--- extract-files.sh
+++ extract-files.sh
@@ -2,6 +2,48 @@
 
 set -e
 
-export VENDOR=lge
-export DEVICE=f300
-./../../$VENDOR/g2-common/extract-files.sh $@
+VENDOR=lge
+DEVICE=f300
+
+INITIAL_COPYRIGHT_YEAR=2013
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+CM_ROOT="$MY_DIR"/../../..
+
+HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -n | --no-cleanup )     CLEAN_VENDOR=false
+                                ;;
+        -s | --section )        shift
+                                SECTION=$1
+                                CLEAN_VENDOR=false
+                                ;;
+        * )                     SRC=$1
+                                ;;
+    esac
+    shift
+done
+
+if [ -z "$SRC" ]; then
+    SRC=adb
+fi
+
+# Initialize the helper for device
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" true $CLEAN_VENDOR
+
+extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+
+"$MY_DIR"/setup-makefiles.sh
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -2,6 +2,32 @@
 
 set -e
 
-export VENDOR=lge
-export DEVICE=f300
-./../../$VENDOR/g2-common/setup-makefiles.sh $@
+VENDOR=lge
+DEVICE=f300
+
+INITIAL_COPYRIGHT_YEAR=2013
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+CM_ROOT="$MY_DIR"/../../..
+
+HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper for device
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" true
+
+# Copyright headers and guards
+write_headers
+
+# The blobs
+write_makefiles "$MY_DIR"/proprietary-files.txt
+
+# We are done with common
+write_footers
